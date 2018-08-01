c45de055cf934422ef513ce1fc731e9cf9a1b790
Andrei Turcu <andrei.george.turcu@gmail.com>
2018-08-01 19:50:14 +0300

    added more templates

--- object_detection.py
+++ object_detection.py
@@ -1,6 +1,7 @@
 import numpy as np
 import time
 import cv2
+import glob
 
 from mss import mss
 
@@ -26,32 +27,44 @@ bbox = (left, up, right, down)
 # trigger for window switching
 # time.sleep(5)
 
+colors = [
+    (255, 0, 0),  # red
+    (0, 255, 0),  # green
+    (0, 0, 255),  # blue
+    (255, 255, 0),  # idk
+    (255, 0, 255),  # idk 2
+    (0, 255, 255),  # idk 3
+]
+
 template = cv2.imread('templates/cacti-1.png', 0)
 
-w, h = template.shape[::-1]
+templates_files = glob.glob('templates\\*.png')
+print(templates_files)
+templates = []
+
+for image in templates_files:
+    image = cv2.imread(image, 0)
+    templates.append(image)
 
 while True:
     sct_img = np.array(sct.grab(monitor))
-    # Grab the data
-
     sct_grey = cv2.cvtColor(sct_img, cv2.COLOR_BGR2GRAY)
 
-    res = cv2.matchTemplate(sct_grey, template, cv2.TM_CCOEFF_NORMED)
-    threshold = 0.9
+    for idx, template in enumerate(templates):
+        w, h = template.shape[::-1]
+        res = cv2.matchTemplate(sct_grey, template, cv2.TM_CCOEFF_NORMED)
+        threshold = 0.95
 
-    loc = np.where( res >= threshold )
+        loc = np.where( res >= threshold )
+        print(*loc)
+        coordinates = zip(*loc[::-1])
+        # print(*coordinates)
 
-    print(loc)
-
-    coordinates = zip(*loc[::-1])
-
-
-    for pt in zip(*loc[::-1]):
-        cv2.rectangle(sct_img, pt, (pt[0] + w, pt[1] + h), (0, 255, 255), 2)
+        for pt in coordinates:
+            cv2.rectangle(sct_img, pt, (pt[0] + w, pt[1] + h), colors[idx], 2)
 
     # cv2.imshow('window', cv2.cvtColor(sct_img, cv2.COLOR_BGR2RGB))
     cv2.imshow('window', sct_img)
-
 
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
