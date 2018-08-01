cbe4a5c6bb2658bd562212d2fb9ac9b9e5365c2c
Andrei Turcu <andrei.george.turcu@gmail.com>
2018-08-01 19:10:29 +0300

    no message

--- object_detection.py
+++ object_detection.py
@@ -1,11 +1,7 @@
-import tensorflow as tf
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import time
-import pyqtgraph as pg
 import cv2
-from PIL import ImageGrab
 
 from mss import mss
 
@@ -31,7 +27,7 @@ bbox = (left, up, right, down)
 # trigger for window switching
 # time.sleep(5)
 
-template = cv2.imread('templates/cacti-1v2.png', 0)
+template = cv2.imread('templates/cacti-1.png', 0)
 
 w, h = template.shape[::-1]
 
@@ -42,7 +38,7 @@ while True:
     sct_grey = cv2.cvtColor(sct_img, cv2.COLOR_BGR2GRAY)
 
     res = cv2.matchTemplate(sct_grey, template, cv2.TM_CCOEFF_NORMED)
-    threshold = 0.7
+    threshold = 0.9
 
     loc = np.where( res >= threshold )
 
@@ -54,7 +50,9 @@ while True:
     for pt in zip(*loc[::-1]):
         cv2.rectangle(sct_img, pt, (pt[0] + w, pt[1] + h), (0, 255, 255), 2)
 
-    cv2.imshow('window', cv2.cvtColor(sct_img, cv2.COLOR_BGR2GRAY))
+    # cv2.imshow('window', cv2.cvtColor(sct_img, cv2.COLOR_BGR2RGB))
+    cv2.imshow('window', sct_img)
+
 
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
